c214efa8b244047fc642f8fedacb2f3495bf5e61
Lee Sangdon <88819145+leeideal@users.noreply.github.com>
2022-08-06 01:09:30 +0900

    F0001-FE Setting & Navbar-LSD
    프론트 초기 세팅, 네브바 제작

---
+++ frontend/src/Routes.js
@@ -0,0 +1,28 @@
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom"
+import Navber from "./components/Navber";
+import Mainpage from "./routes/Mainpage";
+import { ThemeProvider } from 'styled-components';
+import { theme } from './theme';
+import { useRecoilValue } from "recoil";
+import { isMenu } from './atoms';
+import SideMenu from './components/SideMenu';
+
+function Routers(){
+// 메뉴바 클린 check State
+const isMenuClick = useRecoilValue(isMenu);
+console.log(isMenuClick);
+
+return(
+    <Router>
+        <ThemeProvider theme={theme}>
+            <Navber />
+            <Routes>
+                <Route path="/" element={<Mainpage />} />
+            </Routes>
+            {isMenuClick && <SideMenu />}
+        </ThemeProvider>
+    </Router>
+)
+}
+
+export default Routers;
---
+++ frontend/src/routes/Mainpage.jsx
@@ -0,0 +1,7 @@
+function Mainpage() {
+    return (
+        <div style={{marginTop: "10px"}}>메인페이지</div>
+    );
+}
+
+export default Mainpage;
