a14f0177a4080693f06018d967d68058fe33a17a
assayw119 <assayw119@naver.com>
2022-08-10 22:36:45 +0900

    Class 수정

--- backend/post/models.py
+++ backend/post/models.py
@@ -7,11 +7,11 @@ from users.models import User
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
-    writer = models.ForeignKey(User, on_delete=CASCADE)
+    writer = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
-    created_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    sub_content  = models.TextField
+    sub_content  = models.TextField()
     like_users = models.ManyToManyField(User, blank=True, related_name='likes_user', through='Like')
     view_users = models.IntegerField(default=0)
     # chur = # 츄르 모델 참조
@@ -22,9 +22,9 @@ class Post(models.Model):
         return self.title
 
 class Review(models.Model):
-    review_writer = models.ForeignKey(User, on_delete=CASCADE)
-    post_writer = models.ForeignKey(User, on_delete=CASCADE)
-    post = models.OneToOneField(Post, on_delete=CASCADE)
+    writer = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
     rate = models.Choices()
     context = models.TextField()
-    pub_date = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
