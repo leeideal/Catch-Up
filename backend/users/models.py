78f8995fb9405844b9766dbd49a628eceb3412fd
0seoYun <yys020819@gmail.com>
2022-08-11 13:34:58 +0900

    B005 - Profile모델 작업중 - YYS

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,7 +1,10 @@
+from tkinter import CASCADE
 from django.db import models
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 from django.core.validators import MinValueValidator, MaxValueValidator
+#from post.models import Review
+
 
 class User(models.Model):
     id = models.AutoField(primary_key=True)
@@ -14,11 +17,11 @@ class User(models.Model):
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     introduction = models.TextField(null=True)
-    # image= models.ImageField(upload_to = "profile/", blank=True, null=True) # 사용자들이 시간표를 올릴 때마다 media/profile에 저장됨
+    image= models.ImageField(upload_to = "profile/", blank=True, null=True) # 사용자들이 시간표를 올릴 때마다 media/profile에 저장됨
     coin = models.IntegerField(default=100)
     star = models.IntegerField(default=5, validators=[MinValueValidator(0), MaxValueValidator(5)])
     # chat_info = models. # chatting 클래스 참조
-    # reviews = models. # review 모델 참조
+    # reviews = models.ForeignKey(Review, on_delete=models.CASCADE) # review 모델 참조
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
