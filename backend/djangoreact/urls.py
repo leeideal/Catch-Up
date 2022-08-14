f0d5e86b19e258aded299538ae3ad006705f9ec0
taeyeon0319 <xodus1349@likelion.org>
2022-08-14 22:24:44 +0900

    B006-로그인/로그아웃/회원가입-KTY

--- backend/djangoreact/urls.py
+++ backend/djangoreact/urls.py
@@ -20,6 +20,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('post.urls')),
+    path('posts/', include('post.urls')),
     path('users/', include('users.urls')),
+    path('signup/', include('dj_rest_auth.registration.urls')),
+    path('', include('dj_rest_auth.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- backend/users/urls.py
+++ backend/users/urls.py
@@ -3,7 +3,7 @@ from .views import *
 
 app_name = 'users'
 urlpatterns = [
-    path('', user_list_create, name="user_list_create"),
+    path('user_list/', user_list_create, name="user_list_create"),
     path('<int:user_pk>/', user_rud, name="user_rud"),
     path('profile/<int:user_id>', user_profile, name="user_profile"),
     path('profile_update/<int:user_id>', profile_update, name="profile_update"),
