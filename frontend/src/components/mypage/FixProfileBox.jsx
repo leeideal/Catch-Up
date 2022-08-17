d870fb400144b52e746c1f93fcbee7c5f9b64862
Lee Sangdon <88819145+leeideal@users.noreply.github.com>
2022-08-18 02:02:25 +0900

    F019-마이페이지수정API연동및화면수정-LSD

--- frontend/src/components/mypage/FixProfileBox.jsx
+++ frontend/src/components/mypage/FixProfileBox.jsx
@@ -2,7 +2,7 @@ import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import { useForm } from 'react-hook-form';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFilePen } from "@fortawesome/free-solid-svg-icons";
+import { faCirclePlus } from "@fortawesome/free-solid-svg-icons";
 import { LogAPI } from '../../axios';
 import { useEffect, useState } from 'react';
 
@@ -49,9 +49,16 @@ const ProfilePoto= styled.div`
     align-items:center ;
     margin-top: 20px;
     margin-bottom: 30px;
+    position: relative;
 `
 const Poto = styled.input`
-    
+    display: none;
+`
+const Prepoto = styled.img`
+    margin-top: 15px;
+    width : 150px;
+    height: 150px;
+    border-radius: 150px;
 `
 
 const FixItem = styled.div`
@@ -80,12 +87,21 @@ const TextInput = styled.textarea`
     resize: none;
     height: 7em;
 `
+const Icon = styled(FontAwesomeIcon)`
+    font-size: 35px;
+    cursor: pointer;
+    position: absolute;
+    bottom:1px;
+    right:1px;
+    
+`
 
 
 function FixProfileBox () {
     const [info, setInfo] = useState()
     const navigate = useNavigate()
     const {register,  handleSubmit} = useForm();
+    const [poto , setPoto] = useState()
     const getProfile = async() => {
         try{
             const data = await LogAPI.get("/users/myprofile/")
@@ -98,23 +114,54 @@ function FixProfileBox () {
         getProfile()
     },[])
 
+    function previewFile() {
+        var preview = document.querySelector('img');
+        var file = document.querySelector('input[type=file]').files[0];
+        var reader = new FileReader();
+        reader.addEventListener(
+          'load',
+          function () {
+            preview.src = reader.result;
+            setPoto(reader.result);
+          },
+          false
+        );
+        if (file) {
+          reader.readAsDataURL(file);
+        }
+      }
+    
     const onValid = async (data) => {
-        
+        const newData = {
+            "nickname" : data.nickname,
+            "introduction" : data.intro,
+            "image" : poto,
+        }
+        try{
+            await LogAPI.patch('/users/myprofile_update/', newData)
+            navigate("/mypage")
+        } catch(error){
+            console.log(error)
+        }
     } 
     return(
         <Container onSubmit={handleSubmit(onValid)}>
             <Box>
                 {/* API 연동이후 작업하기!! */}
                 <ProfilePoto>
-                    <Poto {...register("img")} type="file"/>
+                    <Prepoto src={info?.profile.image ? info?.profile.image : `https://t1.daumcdn.net/cfile/tistory/2513B53E55DB206927`}/>
+                    <label for="file">
+                        <Icon icon={faCirclePlus}/>
+                    </label>
+                    <Poto id="file" {...register("img")} onChange={previewFile} type="file"/>
                 </ProfilePoto>
                 <FixItem>
                     <FixTitle>닉네임</FixTitle>
-                    <FixInput value={info?.nickname ? info?.nickname : undefined} {...register("nickname" , {required : true,})} placeholder="닉네임을 입력하세요!" />
+                    <FixInput value={info?.profile.nickname ? info?.profile.nickname : undefined} {...register("nickname" , {required : true,})} placeholder="닉네임을 입력하세요!" />
                 </FixItem>
                 <FixItem>
                     <FixTitle>한줄소개</FixTitle>
-                    <TextInput value={info?.introduction ? info?.introduction : undefined} 
+                    <TextInput value={info?.profile.introduction ? info?.profile.introduction : undefined} 
                     {...register("intro" , {required : true,})} placeholder="자신을 소개해주세요!"/>
                 </FixItem>
             </Box>
