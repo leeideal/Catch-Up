7afc498f78d843c525881c3ea2ad3eea9502e85a
ustar1210 <ays991210@naver.com>
2022-08-18 18:14:22 +0900

    B019-채팅방 리스트, 채팅 페이지 api 수정-AYS

--- backend/chat/views.py
+++ backend/chat/views.py
@@ -3,6 +3,7 @@ from .models import *
 from post.models import *
 from users.models import *
 from .serializers import *
+from users.serializers import *
 from rest_framework.response import Response
 from django.shortcuts import render, get_object_or_404
 from rest_framework.decorators import api_view
@@ -26,21 +27,39 @@ def list_chatroom(request):
     if request.method == 'GET':
         current_user = request.user
         chatrooms = Chatroom.objects.all()
-        user_chatrooms = []
+        data = []
         for room in chatrooms:
             if room.is_user_in_talkers(current_user):
-                user_chatrooms.append(room)
-        serializer = ChatroomSerializer(user_chatrooms, many=True)
-        return Response(data=serializer.data)
+                #채팅방 정보
+                roomserializer = ChatroomSerializer(room)   
+                #채팅 상대 정보
+                partner_user = room.opponent(current_user)
+                partner_profile = get_object_or_404(Profile, user=partner_user)
+                opponent_profile_serializer = ProfileSerializer(partner_profile)
+                #가장 최근 채팅 정보
+                chat = Chat.objects.last()
+                chat_serializer = ChatListSerializer(chat)
+                room_chat_set = {
+                    "room":roomserializer.data,
+                    "opponent":opponent_profile_serializer.data,
+                    "last_chat":chat_serializer.data
+                }
+                data.append(room_chat_set)
+        return Response(data=data)
 
 
 @api_view(['GET', 'POST', 'DELETE'])
 def chatlist_roomdelete(request, chatroom_id):
     room = get_object_or_404(Chatroom, id=chatroom_id)
+    room.post.title
     if request.method == 'GET':
         chats = Chat.objects.filter(chatroom = room).order_by('created_at')
-        serializer = ChatListSerializer(chats, many=True)
-        return Response(data=serializer.data)
+        chat_list_serializer = ChatListSerializer(chats, many=True)
+        data = {
+            "topic" : room.post.title,
+            "chat_list":chat_list_serializer.data,
+        }
+        return Response(data=data)
     elif request.method == 'POST':
         current_user = request.user
         serializer = ChatListSerializer(data=request.data)
