bca7624c27c861d2f8d6bf13787f0cd01acd730f
Lee Sangdon <88819145+leeideal@users.noreply.github.com>
2022-08-19 23:46:59 +0900

    F047-후기작성세팅-LSD

--- frontend/src/components/chating/ByeBox.jsx
+++ frontend/src/components/chating/ByeBox.jsx
@@ -1,25 +1,107 @@
 import styled from 'styled-components';
+import { useForm } from 'react-hook-form';
+import StarRatingComponent from 'react-star-rating-component';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { LogAPI } from '../../axios';
 
 const Wapper = styled.div`
     width: 100%;
     height: 100%;
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
     display: flex;
-    flex-direction: column;
-    
+    flex-direction: column; 
+    align-items: center;
+    color : rgb(24,62,78);
 `
 
 const Title = styled.h1`
     font-size: 28px;
     font-weight: 700;
+    width: 80%;
+    margin-top: 50px;
+`
+
+const SubTitle = styled.h5`
+    font-size: 20px;
+    font-weight: 600;
+    width: 80%;
+    margin-top: 20px;
+    margin-bottom: 20px;
+`
+
+const Form = styled.form`
+    width: 80%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
+const Input = styled.textarea`
+    width: 100%;
+    padding: 12px 12px;
+    border-radius: 10px;
+    border: none;
+    margin-top: 3px;
+    height: 200px;
+    background-color: #F5F5F5;
+    resize: none;
+`
+const Star = styled(StarRatingComponent)`
+    font-size: 40px;
+    cursor: pointer;
+    margin-bottom: 30px;
+`
+const Btn = styled.button`
+    border: none;
+    background-color: #68DCC4;
+    font-size: 20px;
+    font-weight: 600;
+    padding: 15px 15px;
+    margin-top: 20px;
+    border-radius: 10px;
+    cursor: pointer;
 `
 
 function ByeBox() {
+    const {register, handleSubmit} = useForm()
+    const [star, setStar] = useState(0);
+    const nav = useNavigate()
+
+    const onStar = (nextValue, prevValue, name) => {
+        setStar(Math.round(nextValue));
+    }
+    const onValid = async(data) => {
+        const newData = {
+            "rate" : star,
+            "content" : data.content
+        }
+        await LogAPI.post("" ,newData);
+        nav("/chat")
+    }
     return(
         <Wapper>
             <Title>
                 후기쓰기
             </Title>
+            <SubTitle>
+                채팅에 만족하셨나요?
+            </SubTitle>
+            <Star
+                name="rate1" 
+                starCount={5}
+                value={star}
+				starColor={"rgb(24,62,78)"}
+                emptyStarColor={"rgba(0,0,0,0.3)"}
+                onStarClick={onStar.bind(this)}
+            />
+            <SubTitle>
+            내용을 작성해주세요
+            </SubTitle>
+            <Form onSubmit={handleSubmit(onValid)}>
+            <Input {...register("content", {required:true})} placeholder="다른이들에게 공유하고 싶은 자신만의 이야기를 써보세요!" />
+            <Btn>후기등록</Btn>
+            </Form>
         </Wapper>
     );
 }
