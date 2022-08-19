ca7fed6ff135c5b71be40fe9132130075c40fe00
Lee Sangdon <88819145+leeideal@users.noreply.github.com>
2022-08-19 21:09:25 +0900

    F045-채팅 리스트-LSD

--- backend/chat/views.py
+++ backend/chat/views.py
@@ -8,31 +8,32 @@ from rest_framework.response import Response
 from django.shortcuts import render, get_object_or_404
 from rest_framework.decorators import api_view
 
+
 @api_view(['POST'])
 def create_chatroom(request):
-    #POST방식일 때 채팅방 생성
+    # POST방식일 때 채팅방 생성
     if request.method == 'POST':
         current_post = get_object_or_404(Post, pk=request.data['post_id'])
         current_user = request.user
         current_profile = get_object_or_404(Profile, user=current_user)
-        #채팅방, 질문자가 같은 방이 있으면 새로 생성하지 않고 기존의 채팅방을 반환
+        # 채팅방, 질문자가 같은 방이 있으면 새로 생성하지 않고 기존의 채팅방을 반환
         result = 0
-        if Chatroom.objects.filter(post = current_post):
-            first = Chatroom.objects.filter(post = current_post)
+        if Chatroom.objects.filter(post=current_post):
+            first = Chatroom.objects.filter(post=current_post)
             result = 1
-            if first.filter(questioner = request.user):
-                second = first.filter(questioner = request.user)
+            if first.filter(questioner=request.user):
+                second = first.filter(questioner=request.user)
                 chatroom_serializer = ChatroomSerializer(second, many=True)
-                result=2
-            #채팅방이 없으면? 새로 만들어야지
+                result = 2
+            # 채팅방이 없으면? 새로 만들어야지
         if result == 2:
             data = {
-                "is_chure_valid":3,
-                "chatroom":chatroom_serializer.data
+                "is_chure_valid": 3,
+                "chatroom": chatroom_serializer.data
             }
             return Response(data=data)
         else:
-            #내가 가진 츄르가 채팅방 걸수 있는 개수면 츄르개수 조정
+            # 내가 가진 츄르가 채팅방 걸수 있는 개수면 츄르개수 조정
             if current_profile.churu >= current_post.churu:
                 current_profile.churu = current_profile.churu - current_post.churu
                 print(current_profile.churu)
@@ -41,22 +42,22 @@ def create_chatroom(request):
                 chatroom_serializer = ChatroomSerializer(data=request.data)
                 if chatroom_serializer.is_valid(raise_exception=True):
                     chatroom_serializer.save(
-                        post = current_post, 
-                        questioner = current_user,
-                        answerer = current_post.writer)
+                        post=current_post,
+                        questioner=current_user,
+                        answerer=current_post.writer)
                 data = {
-                    "is_chure_valid":is_chure_valid,
-                    "chatroom":chatroom_serializer.data
+                    "is_chure_valid": is_chure_valid,
+                    "chatroom": chatroom_serializer.data
                 }
                 return Response(data=data)
-            else :
+            else:
                 is_chure_valid = 0
                 data = {
-                    "is_chure_vallid":is_chure_valid,
-                    "chatroom":"채팅룸이 만들어지지 않았어요 흑흑"
+                    "is_chure_vallid": is_chure_valid,
+                    "chatroom": "채팅룸이 만들어지지 않았어요 흑흑"
                 }
                 return Response(data=data)
-        
+
 
 @api_view(['GET'])
 def list_chatroom(request):
@@ -66,23 +67,30 @@ def list_chatroom(request):
         data = []
         for room in chatrooms:
             if room.is_user_in_talkers(current_user):
-                #채팅방 정보
-                roomserializer = ChatroomSerializer(room)   
-                #채팅 상대 정보
+                # 채팅방 정보
+                roomserializer = ChatroomSerializer(room)
+                # 채팅 상대 정보
                 partner_user = room.opponent(current_user)
                 partner_profile = get_object_or_404(Profile, user=partner_user)
-                opponent_profile_serializer = ProfileSerializer(partner_profile)
+                opponent_profile_serializer = ProfileSerializer(
+                    partner_profile)
                 room_chat_set = {
-                    "room":roomserializer.data,
-                    "opponent":opponent_profile_serializer.data
+                    "topic": room.post.title,
+                    "room": roomserializer.data,
+                    "opponent": opponent_profile_serializer.data
                 }
-                #가장 최근 채팅 정보
+                # 가장 최근 채팅 정보
                 if Chat.objects.filter(chatroom=room):
                     chat = Chat.objects.filter(chatroom=room)
                     chat = chat.last()
                     chat_serializer = ChatListSerializer(chat)
                     room_chat_set['last_chat'] = chat_serializer.data
+                else:
+                    chat = Chat()
+                    chat_serializer = ChatListSerializer(chat)
+                    room_chat_set['last_chat'] = chat_serializer.data
                 data.append(room_chat_set)
+
         return Response(data=data)
 
 
@@ -91,18 +99,18 @@ def chatlist_roomdelete(request, chatroom_id):
     room = get_object_or_404(Chatroom, id=chatroom_id)
     current_user = request.user
     if request.method == 'GET':
-        chats = Chat.objects.filter(chatroom = room).order_by('-created_at')
+        chats = Chat.objects.filter(chatroom=room).order_by('created_at')
         chat_list = []
         chat_user_set = {}
         for chat in chats:
             chat_serializer = ChatListSerializer(chat)
             if chat.writer == request.user:
                 is_user = 1
-            else :
+            else:
                 is_user = 0
             chat_user_set = {
-                "chat":chat_serializer.data,
-                "is_user":is_user
+                "chat": chat_serializer.data,
+                "is_user": is_user
             }
             chat_list.append(chat_user_set)
 
@@ -110,23 +118,22 @@ def chatlist_roomdelete(request, chatroom_id):
         partner_profile = get_object_or_404(Profile, user=partner_user)
         opponent_profile_serializer = ProfileSerializer(partner_profile)
         data = {
-            "topic" : room.post.title,
-            "opponent":opponent_profile_serializer.data,
-            "chat_list":chat_list,
+            "topic": room.post.title,
+            "opponent": opponent_profile_serializer.data,
+            "chat_list": chat_list,
         }
         return Response(data=data)
     elif request.method == 'POST':
         serializer = ChatListSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
             serializer.save(
-                chatroom = room,
-                writer = current_user, 
-                content = request.data['content'])
+                chatroom=room,
+                writer=current_user,
+                content=request.data['content'])
             return Response(data=serializer.data)
     elif request.method == 'DELETE':
         room.delete()
-        data={
-            'chatroom':chatroom_id
+        data = {
+            'chatroom': chatroom_id
         }
         return Response(data)
-
