916e2d95d29cc5d9b5f1074e7c06e32840e63f1b
assayw119 <assayw119@naver.com>
2022-08-20 05:04:41 +0900

    proxy 로컬로 수정

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function (app) {
   app.use(
     createProxyMiddleware('/api', {
-      target: 'http://catch-up.site:8000',
+      target: 'http://localhost:8000',
       changeOrigin: true,
     }),
   );
